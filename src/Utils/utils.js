29c98277db80e997071bf834a6a53b39631e6fbc
Hamza Afridi <hamza.afridi@vetdrive.co>
2024-02-07 11:11:46 +0000

    message update

--- src/Utils/utils.js
+++ src/Utils/utils.js
@@ -6,7 +6,9 @@ export default function checkTwoThirdVAT(totalProductInclVAT, totalServiceInclVA
     const isTwoThirdValid = totalProductExVat < cutoff;
     const message = `
         Hi, \n
-        the vat exempt total of invoice is: ${totalProductExVat.toFixed(2)}+ ${totalServiceExVat.toFixed(2)} = ${totalInvoiceExVat.toFixed(2)} \n
+        the ex vat total of product is: ${totalProductExVat.toFixed(2)} \n
+        the ex vat total of service is: ${totalServiceExVat.toFixed(2)} \n
+        the ec exempt total of invoice is: ${totalProductExVat.toFixed(2)}+ ${totalServiceExVat.toFixed(2)} = ${totalInvoiceExVat.toFixed(2)} \n
         and the cutoff is: ${totalInvoiceExVat.toFixed(2)} x 2 / 3 = ${cutoff.toFixed(2)} \n
         as a result, the total of product is ${totalProductExVat.toFixed(2)} ${isTwoThirdValid ? "<" : ">" } ${cutoff.toFixed(2)} \n
         so two third vat rule is ${isTwoThirdValid ? "VALID": "INVALID"} \n
